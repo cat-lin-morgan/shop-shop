b058abeff37d6e2732933e733c42c71fe13a4044
cat - lin <plummorgan@gmail.com>
2021-01-20 23:50:07 -0800

    adding context api

---
+++ client/src/utils/GlobalState.js
@@ -0,0 +1,48 @@
+import React, { createContext, useContext } from "react";
+import { useProductReducer } from './reducers';
+
+const StoreContext = createContext();
+const { Provider } = StoreContext;
+
+const StoreProvider = ({ value = [], ...props }) => {
+    const [state, dispatch] = useProductReducer({
+        products: [],
+        categories: [],
+        currentCategory: '',
+    })
+    console.log(state);
+    return <Provider value={[state, dispatch]} {...props} />;
+};
+
+const useStoreContext = () => {
+    return useContext(StoreContext);
+}
+
+export {StoreProvider, useStoreContext};
+
+
+// props = {
+//     a: a,
+//     b: b,
+//     c: c,
+// };
+
+// <Example {...props} />
+
+// <Example
+//     a={props.a}
+//     b={props.b}
+//     c={props.c}
+// />
+
+
+// <Example props={props} />
+
+
+// const Example = (props) => {
+//     props = {
+//         props: {
+//             a: a,
+//         }
+//     }
+// }
