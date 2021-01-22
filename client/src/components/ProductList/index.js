c75126a85c46ad3de6cbd2f515d79b9b9da2fd85
cat - lin <plummorgan@gmail.com>
2021-01-22 14:12:10 -0800

    adding indexeddb to cart

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -8,6 +8,8 @@ import ProductItem from "../ProductItem";
 import { QUERY_PRODUCTS } from "../../utils/queries";
 import spinner from "../../assets/spinner.gif"
 
+import { idbPromise } from "../../utils/helpers";
+
 function ProductList() {
   const [state, dispatch] = useStoreContext();
   const { loading, data } = useQuery(QUERY_PRODUCTS);
@@ -19,9 +21,19 @@ function ProductList() {
       dispatch({
         type: UPDATE_PRODUCTS,
         products: data.products
+      });
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      })
+    } else if (!loading){
+      idbPromise('products', 'get').then((products) => {
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products, // same as products: products shorthand pbject assignment
+        })
       })
     }
-  }, [data, dispatch]);
+  }, [data, loading, dispatch]);
 
   function filterProducts() {
     if (!currentCategory) {
--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -4,3 +4,65 @@ export function pluralize(name, count) {
   }
   return name + 's'
 }
+
+export function idbPromise(storeName, method, object) {
+  return new Promise((resolve, reject) => {
+    const request = window.indexedDB.open('shop-shop', 1);
+
+    let db, tx, store;
+
+    request.onupgradeneeded = function(e) {
+      const db = request.result;
+      // create object store for each type of data and set "primary" key index to be the `_id` of the data
+      db.createObjectStore('products', { keyPath: '_id' });
+      db.createObjectStore('categories', { keyPath: '_id' });
+      db.createObjectStore('cart', { keyPath: '_id' });
+    };
+
+
+    request.onerror = function(e) {
+      console.log('There was at one point recently an error.');
+    };
+
+    request.onsuccess = function (e) {
+      // save a reference of the database to the `db` variable
+      db = request.result;
+      // open a transaction do whatever we pass into `storeName` (must match one of the object store names)
+      tx = db.transaction(storeName, 'readwrite');
+      // save a reference to that object store
+      store = tx.objectStore(storeName);
+
+      // if there's any errors, let us know
+      db.onerror = function(e) {
+        console.log('error', e);
+      };
+
+      switch(method) {
+        case 'put':
+          store.put(object);
+          resolve(object);
+          break;
+        case 'get':
+          const all = store.getAll();
+          all.onsuccess = function() {
+            resolve(all.result);
+          };
+          break;
+        case 'delete':
+          store.delete(object._id);
+          break;
+        default:
+          console.log('No method for you');
+          break;
+      }
+
+      tx.oncomplete = function (e) {
+        db.close();
+      };
+
+    };
+
+  });
+
+}
+
